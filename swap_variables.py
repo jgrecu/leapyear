2265f50538d54fefc52321583631d7756c20e1be
Jeremy Grecu <jgrecu@gmail.com>
2021-01-01 22:50:44 +0100

    function added

--- swap_variables.py
+++ swap_variables.py
@@ -1,15 +1,14 @@
 # swap a value with b value without using a third variable
 
-# logical way
 a = int(input("Input value of a: "))
 b = int(input("Input value of b: "))
 
 print(f"A = {a} and B = {b}")
+# logical way
 # a = a + b
 # b = a - b
 # a = a - b
 # print(f"A = {a} and B = {b}")
-
 
 # python function way
 def swap(a, b):
