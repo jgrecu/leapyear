df220293ec79aa4445df7d5972da86c20e23d07d
Jeremy Grecu <74504061+jgrecu@users.noreply.github.com>
2021-01-24 16:24:13 +0000

    final version of the WordCloud app

--- word_cloud.py
+++ word_cloud.py
@@ -1,15 +1,30 @@
 # from wordcloud import WordCloud, STOPWORDS
 import wordcloud
+from matplotlib import pyplot as plt
 
-uninteresting_words = ["a", "an", "at", "or", "by", "the", "to", "if", "of"]
 with open('The_Call_of_the_Wild.txt') as f:
-    words = f.read()
+    file_contents = f.read()
+
+
+def calculate_frequencies(file_contents):
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    # uninteresting_words = ["a", "an", "at", "or", "by", "the", "to", "if", "of"]
     frequencies = {}
-    for word in words.lower().split():
+    for word in file_contents.lower().split():
         if word.isalpha() and word not in uninteresting_words:
             frequencies[word] = frequencies.setdefault(word, 0) + 1
-# print(frequencies)
+    # print(frequencies)
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(frequencies)
+    # cloud.to_file("myfile.jpg")
+    return cloud.to_array()
 
-cloud = wordcloud.WordCloud()
-cloud.generate_from_frequencies(frequencies)
-cloud.to_file("myfile.jpg")
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
