1b0a614f56ac620e53513c21824d32e8c1a7e431
Jeremy Grecu <jeremy.grecu@sap.com>
2021-03-05 15:44:11 +0000

    Updated is_prime function

--- prime.py
+++ prime.py
@@ -1,16 +1,16 @@
 def is_prime(num):
     """ Return True is the number is prime. """
-    prime = True
+    if num < 2:
+        return False
     for n in range(2, num):
         if num % n == 0:
-            # print(f"{num} % {n} = {num%n}")
-            prime = False
-    return prime
+            return False
+    return True
 
-def primes_lessthan(n):
+def primes_lessthan(number):
     """ Return a list of all prime numbers less than n. """
     result = []
-    for i in range(2,n):
+    for i in range(2, number):
         if is_prime(i):
             result.append(i)
     return result
