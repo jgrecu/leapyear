2d66d3caf23e3409981e66a704f1d4d8e2508a96
Jeremy Grecu <jeremy.grecu@sap.com>
2021-04-26 10:34:08 +0100

    Changed the name of the method to make it clearer what it does.

--- LeapYear.java
+++ LeapYear.java
@@ -1,10 +1,10 @@
 public class LeapYear {
 
     public static void main(String[] args) {
-        System.out.println(isLeap(2020));
+        System.out.println(isLeapYear(2020));
     }
 
-    public static boolean isLeap(int year) {
+    public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
             if (year % 400 == 0) {
                 return true;
